fc553b5e6819c21646a94fa05bdd50a530602333
giogua03 <giorgiogua03@gmail.com>
2023-01-25 18:34:15 +0100

    problem con server

---
+++ app/Post.php
@@ -0,0 +1,12 @@
+nes (7 sloc)  100 Bytes
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    Ciao {{ $user->name }}, la tua email è: {{ $user->email }}
+
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Sei loggato') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
